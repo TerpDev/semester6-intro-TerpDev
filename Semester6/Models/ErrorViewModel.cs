234270eb27ae88d384ad23a2a02664abf86d03b7
Daniel Terpstra <d.terpstra@c-o-d-e-s.com>
2025-03-25 10:17:36 +0100

    made project

---
+++ Semester6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Semester6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
